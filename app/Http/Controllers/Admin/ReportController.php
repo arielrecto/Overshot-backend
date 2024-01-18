fe430552f0994e040f0ff97eed25e980d20fd0a6
arielrecto <arielrecto29@gmail.com>
2024-01-19 01:27:41 +0800

    update:sales report

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -12,12 +12,11 @@ class ReportController extends Controller
     public function __construct(
         public Transaction $transaction,
         public Order $order
-        )
-    {
-
+    ) {
     }
 
-    public function transaction(){
+    public function transaction()
+    {
 
         $transactions = $this->transaction->get();
 
@@ -29,11 +28,20 @@ class ReportController extends Controller
 
 
         return response($data, 200);
-
     }
-    public function sales(){
+    public function sales()
+    {
 
-        $orders = $this->order->withCount(['products'])->get();
+        // $orders = $this->order->with('cart')->withCount('cart.cartProducts')->get();
+
+
+        $orders = $this->order->with(['cart' => function ($q) {
+            $q->withCount('cartProducts');
+        }])
+            ->whereHas('cart', function ($q) {
+                $q->where('is_check_out', true);
+            })
+            ->get();
 
         $data = [
             'monthlySales' => $this->monthlySales(now()->year),
@@ -41,19 +49,19 @@ class ReportController extends Controller
         ];
 
         return response($data, 200);
-
     }
 
-    private function monthlyTransaction($year){
+    private function monthlyTransaction($year)
+    {
 
         $monthlyTotal = [];
 
 
-        for ($month = 1; $month <= 12; $month++){
+        for ($month = 1; $month <= 12; $month++) {
 
             $totalTransaction = Transaction::whereMonth('created_at', $month)
-                                ->whereYear('created_at', $year)
-                                ->count();
+                ->whereYear('created_at', $year)
+                ->count();
 
             $fullBNameOfMonth = date('F', strtotime("{$year}-{$month}-1"));
 
@@ -63,23 +71,51 @@ class ReportController extends Controller
 
         return $monthlyTotal;
     }
-    private function monthlySales($year){
-
+    private function monthlySales($year)
+    {
         $monthlyTotal = [];
 
+        for ($month = 1; $month <= 12; $month++) {
+            $orders = Order::whereMonth('created_at', $month)
+                ->whereYear('created_at', $year)
+                ->with(['cart' => function ($q) {
+                    $q->where('is_check_out', true);
+                }])
+                ->get();
 
-        for ($month = 1; $month <= 12; $month++){
-
-            $totalTransaction = Order::whereMonth('created_at', $month)
-                                ->whereYear('created_at', $year)
-                                ->sum('total');
+            $totalTransaction = $orders->sum(function ($order) {
+                return optional($order->cart)->sum('total') ?? 0;
+            });
 
             $fullBNameOfMonth = date('F', strtotime("{$year}-{$month}-1"));
 
             $monthlyTotal[$fullBNameOfMonth] = $totalTransaction;
         }
 
-
         return $monthlyTotal;
     }
+    // private function monthlySales($year)
+    // {
+
+    //     $monthlyTotal = [];
+
+
+    //     for ($month = 1; $month <= 12; $month++) {
+
+    //         $totalTransaction = Order::whereMonth('created_at', $month)
+    //             ->whereYear('created_at', $year)
+    //             ->with(['cart' => function($q){
+    //                 $q->where('is_check_out', true);
+    //             }])  // Assuming there is a relationship between Order and Cart
+    //             ->sum('cart.total');
+
+
+    //         $fullBNameOfMonth = date('F', strtotime("{$year}-{$month}-1"));
+
+    //         $monthlyTotal[$fullBNameOfMonth] = $totalTransaction;
+    //     }
+
+
+    //     return $monthlyTotal;
+    // }
 }
